def690d4585fae01b89a9ae20b34d0722fce66de
Sandip Kale <kalesandip42@gmail.com>
2026-08-29 18:00:13 +0530

    jenkins pipeline

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
     public static void main(String[] args) {
-        System.out.println("Welcome to Java session.My name is sandy.I am very clever");
+        System.out.println("Hello world ");
     }
 }
