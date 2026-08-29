8b08de780b2d2df357a20e146df682e2f536610b
Sandip Kale <kalesandip42@gmail.com>
2026-08-29 19:32:55 +0530

    cron job scheduled through Poll SCM

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
     public static void main(String[] args) {
-        System.out.println("Hello world. my first jenkins freestyle pipeline successfully ran ");
+        System.out.println("cron job scheduled through poll SCM  ");
     }
 }
